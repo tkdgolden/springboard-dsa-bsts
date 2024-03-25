c46caa96903aa007d11d8b5c1c9d10366ecb1d0a
Olivia Golden <olivia.golden.w@gmail.com>
2024-03-25 17:54:18 -0500

    submit

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -15,56 +15,175 @@ class BinarySearchTree {
    * Returns the tree. Uses iteration. */
 
   insert(val) {
+    if (this.root === null) {
+      this.root = new Node(val);
+      return this;
+    }
 
+    let current = this.root;
+    while (true) {
+      if (val < current.val) {
+        if (current.left === null) {
+          current.left = new Node(val);
+          return this;
+        }
+        else {
+          current = current.left;
+        }
+      }
+      else if (val > current.val) {
+        if (current.right === null) {
+          current.right = new Node(val);
+          return this;
+        }
+        else {
+          current = current.right;
+        }
+      }
+    }
   }
 
   /** insertRecursively(val): insert a new node into the BST with value val.
    * Returns the tree. Uses recursion. */
 
-  insertRecursively(val) {
-
+  insertRecursively(val, current = this.root) {
+    if (this.root === null) {
+      this.root = new Node(val);
+      return this;
+    }
+    if (val > current.val) {
+      if (current.right === null) {
+        current.right = new Node(val);
+        return this;
+      }
+      return this.insertRecursively(val, current.right);
+    }
+    else {
+      if (current.left === null) {
+        current.left = new Node(val);
+        return this;
+      }
+      return this.insertRecursively(val, current.left);
+    }
   }
 
   /** find(val): search the tree for a node with value val.
    * return the node, if found; else undefined. Uses iteration. */
 
   find(val) {
-
+    let current = this.root;
+    while(true) {
+      if (current === null) {
+        return undefined;
+      }
+      if (current.val === val) {
+        return current;
+      }
+      if (current.val > val) {
+        current = current.left;
+      }
+      else {
+        current = current.right;
+      }
+    }
   }
 
   /** findRecursively(val): search the tree for a node with value val.
    * return the node, if found; else undefined. Uses recursion. */
 
-  findRecursively(val) {
-
+  findRecursively(val, current=this.root) {
+    if (current === null) {
+      return undefined;
+    }
+    if (current.val === val) {
+      return current;
+    }
+    if (current.val > val) {
+      return this.findRecursively(val, current.left);
+    }
+    else {
+      return this.findRecursively(val, current.right);
+    }
   }
 
   /** dfsPreOrder(): Traverse the array using pre-order DFS.
    * Return an array of visited nodes. */
 
   dfsPreOrder() {
+    const visited = [];
 
+    function traverse(node) {
+      visited.push(node.val);
+      if (node.left) {
+        traverse(node.left);
+      }
+      if (node.right) {
+        traverse(node.right);
+      }
+    }
+    traverse(this.root);
+    return visited;
   }
 
   /** dfsInOrder(): Traverse the array using in-order DFS.
    * Return an array of visited nodes. */
 
   dfsInOrder() {
+    const visited = [];
 
+    function traverse(node) {
+      if (node.left) {
+        traverse(node.left);
+      }
+      visited.push(node.val);
+      if (node.right) {
+        traverse(node.right);
+      }
+    }
+
+    traverse(this.root);
+    return visited;
   }
 
   /** dfsPostOrder(): Traverse the array using post-order DFS.
    * Return an array of visited nodes. */
 
   dfsPostOrder() {
+    const visited = [];
 
+    function traverse(node) {
+      if (node.left) {
+        traverse(node.left);
+      }
+      if (node.right) {
+        traverse(node.right);
+      }
+      visited.push(node.val);
+    }
+    traverse(this.root);
+    return visited;
   }
 
   /** bfs(): Traverse the array using BFS.
    * Return an array of visited nodes. */
 
   bfs() {
-
+    const visited = [];
+    const toDo = [];
+    function traverse(node) {
+      visited.push(node.val);
+      if (node.left) {
+        toDo.unshift(node.left);
+      }
+      if (node.right) {
+        toDo.unshift(node.right);
+      }
+    }
+    traverse(this.root);
+    while(toDo.length > 0) {
+      traverse(toDo.pop())
+    }
+    return visited;
   }
 
   /////////////////////////////////////////////////////////////////////
